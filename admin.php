41622750d4433d817d05ae7d2b65113cc0ccd896
winiaa <winiaaradawd14@gmail.com>
2023-11-16 00:55:06 +0700

    I have added register, login and logout. and i also have fixed some bugs

--- admin.php
+++ admin.php
@@ -3,6 +3,14 @@
 
 @include 'config.php';
 
+session_start();
+
+if(!isset($_SESSION['admin_name'])){
+    header('location:login_form.php');
+}
+
+
+
 $message = [];
 
 // Here need to add a Product
@@ -62,31 +70,31 @@ if (isset($_POST['update_product'])) {
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>admin panel</title>
+   <meta charset="UTF-8">
+   <meta http-equiv="X-UA-Compatible" content="IE=edge">
+   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   <title>admin panel</title>
 
-    <!-- font awesome cdn link  -->
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css">
+   <!-- font awesome cdn link  -->
+   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css">
 
-    <!-- custom css file link  -->
-    <link rel="stylesheet" href="css/style.css">
+   <!-- custom css file link  -->
+   <link rel="stylesheet" href="css/style.css">
 
 </head>
 <body>
-
+   
 <?php
 
 if(isset($message)){
-    foreach($message as $message){
-        echo '<div class="message"><span>'.$message.'</span> <i class="fas fa-times" onclick="this.parentElement.style.display = `none`;"></i> </div>';
-    };
+   foreach($message as $message){
+      echo '<div class="message"><span>'.$message.'</span> <i class="fas fa-times" onclick="this.parentElement.style.display = `none`;"></i> </div>';
+   };
 };
 
 ?>
 
-<header class="header">
+<header class="header" style="background-color:var(--black)">
 
     <div class="flex">
 
@@ -95,6 +103,14 @@ if(isset($message)){
         <nav class="navbar">
 
         </nav>
+
+        <div class="dropdown">
+            <button class="dropbtn">
+                <a href="logout.php" class="btn">logout</a>
+
+            </button>
+
+        </div>
 
 
 
@@ -106,93 +122,105 @@ if(isset($message)){
 
 <div class="container">
 
-    <section>
+<section>
 
-        <form action="" method="post" class="add-product-form" enctype="multipart/form-data">
-            <h3>add a new product</h3>
-            <input type="text" name="p_name" placeholder="enter the product name" class="box" required>
-            <input type="number" name="p_price" min="0" placeholder="enter the product price" class="box" required>
-            <input type="file" name="p_image" accept="image/png, image/jpg, image/jpeg" class="box" required>
-            <input type="submit" value="add the product" name="add_product" class="btn">
-        </form>
+<form action="" method="post" class="add-product-form" enctype="multipart/form-data">
+   <h3>add a new product</h3>
+   <input type="text" name="p_name" placeholder="enter the product name" class="box" required>
+   <input type="number" name="p_price" min="0" placeholder="enter the product price" class="box" required>
+   <input type="file" name="p_image" accept="image/png, image/jpg, image/jpeg" class="box" required>
+   <input type="submit" value="add the product" name="add_product" class="btn">
+</form>
 
-    </section>
+</section>
 
-    <section class="display-product-table">
+<section class="display-product-table">
 
-        <table>
+   <table>
 
-            <thead>
-            <th>product image</th>
-            <th>product name</th>
-            <th>product price</th>
-            <th>action</th>
-            </thead>
+      <thead>
+         <th>product image</th>
+         <th>product name</th>
+         <th>product price</th>
+         <th>action</th>
+      </thead>
 
-            <tbody>
-            <?php
-
+      <tbody>
+         <?php
+         
             $select_products = mysqli_query($conn, "SELECT * FROM `products`");
             if(mysqli_num_rows($select_products) > 0){
-                while($row = mysqli_fetch_assoc($select_products)){
-                    ?>
+               while($row = mysqli_fetch_assoc($select_products)){
+         ?>
 
-                    <tr>
-                        <td><img src="uploaded_img/<?php echo $row['image']; ?>" height="100" alt=""></td>
-                        <td><?php echo $row['name']; ?></td>
-                        <td>$<?php echo $row['price']; ?>/-</td>
-                        <td>
-                            <a href="admin.php?delete=<?php echo $row['id']; ?>" class="delete-btn" onclick="return confirm('are your sure you want to delete this?');"> <i class="fas fa-trash"></i> delete </a>
-                            <a href="admin.php?edit=<?php echo $row['id']; ?>" class="option-btn"> <i class="fas fa-edit"></i> update </a>
-                        </td>
-                    </tr>
+         <tr>
+            <td><img src="uploaded_img/<?php echo $row['image']; ?>" height="100" alt=""></td>
+            <td><?php echo $row['name']; ?></td>
+            <td>$<?php echo $row['price']; ?>/-</td>
+            <td>
+               <a href="admin.php?delete=<?php echo $row['id']; ?>" class="delete-btn" onclick="return confirm('are your sure you want to delete this?');"> <i class="fas fa-trash"></i> delete </a>
+               <a href="admin.php?edit=<?php echo $row['id']; ?>" class="option-btn"> <i class="fas fa-edit"></i> update </a>
+            </td>
+         </tr>
 
-                    <?php
-                };
+         <?php
+            };    
             }else{
-                echo "<div class='empty'>no product added</div>";
+               echo "<div class='empty'>no product added</div>";
             };
-            ?>
-            </tbody>
-        </table>
+         ?>
+      </tbody>
+   </table>
 
-    </section>
+</section>
 
-    <section class="edit-form-container">
+<section class="edit-form-container">
 
-        <?php
+   <?php
+   
+   if(isset($_GET['edit'])){
+      $edit_id = $_GET['edit'];
+      $edit_query = mysqli_query($conn, "SELECT * FROM `products` WHERE id = $edit_id");
+      if(mysqli_num_rows($edit_query) > 0){
+         while($fetch_edit = mysqli_fetch_assoc($edit_query)){
+   ?>
 
-        if(isset($_GET['edit'])){
-            $edit_id = $_GET['edit'];
-            $edit_query = mysqli_query($conn, "SELECT * FROM `products` WHERE id = $edit_id");
-            if(mysqli_num_rows($edit_query) > 0){
-                while($fetch_edit = mysqli_fetch_assoc($edit_query)){
-                    ?>
+   <form action="" method="post" enctype="multipart/form-data">
+      <img src="uploaded_img/<?php echo $fetch_edit['image']; ?>" height="200" alt="">
+      <input type="hidden" name="update_p_id" value="<?php echo $fetch_edit['id']; ?>">
+      <input type="text" class="box" required name="update_p_name" value="<?php echo $fetch_edit['name']; ?>">
+      <input type="number" min="0" class="box" required name="update_p_price" value="<?php echo $fetch_edit['price']; ?>">
+      <input type="file" class="box" required name="update_p_image" accept="image/png, image/jpg, image/jpeg">
+      <input type="submit" value="update the prodcut" name="update_product" class="btn">
+      <input type="reset" value="cancel" id="close-edit" class="option-btn">
+   </form>
 
-                    <form action="" method="post" enctype="multipart/form-data">
-                        <img src="uploaded_img/<?php echo $fetch_edit['image']; ?>" height="200" alt="">
-                        <input type="hidden" name="update_p_id" value="<?php echo $fetch_edit['id']; ?>">
-                        <input type="text" class="box" required name="update_p_name" value="<?php echo $fetch_edit['name']; ?>">
-                        <input type="number" min="0" class="box" required name="update_p_price" value="<?php echo $fetch_edit['price']; ?>">
-                        <input type="file" class="box" required name="update_p_image" accept="image/png, image/jpg, image/jpeg">
-                        <input type="submit" value="update the prodcut" name="update_product" class="btn">
-                        <input type="reset" value="cancel" id="close-edit" class="option-btn">
-                    </form>
-
-                    <?php
-                };
+   <?php
             };
-            echo "<script>document.querySelector('.edit-form-container').style.display = 'flex';</script>";
-        };
-        ?>
+         };
+         echo "<script>document.querySelector('.edit-form-container').style.display = 'flex';</script>";
+      };
+   ?>
 
-    </section>
+</section>
 
 </div>
 
 
 
-<!-- Here need to custom js file link  -->
+
+
+
+
+
+
+
+
+
+
+
+
+<!-- custom js file link  -->
 <script src="js/script.js"></script>
 
 
--- header.php
+++ header.php
@@ -1,4 +1,4 @@
-<header class="header">
+<header class="header" style="background-color:var(--black)">
 
    <div class="flex">
 
@@ -8,7 +8,11 @@
 
           <a href="#">Home</a>
          <a href="products.php">Shop</a>
-          <a href="#">About</a>
+          <a href="#">About Us</a>
+
+
+
+
       </nav>
 
       <?php
@@ -20,7 +24,20 @@
 
       <a href="cart.php" class="cart">cart <span><?php echo $row_count; ?></span> </a>
 
+
       <div id="menu-btn" class="fas fa-bars"></div>
+
+       <div class="dropdown">
+           <button class="dropbtn">
+               Login
+               <i class="fa fa-caret-down"></i>
+           </button>
+           <div class="dropdown-content">
+               <a href="login_form.php" class="bt">login</a>
+               <a href="register_form.php" class="bt">register</a>
+               <a href="logout.php" class="bt">logout</a>
+           </div>
+       </div>
 
    </div>
 

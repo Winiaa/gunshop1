557ab86ed43dbab5bfb8090e7a944df72dde0cde
winiaa <winiaaradawd14@gmail.com>
2023-12-11 17:38:44 +0700

    I have improved orders file to show the orders items in the admin part

--- admin/orders.php
+++ admin/orders.php
@@ -13,60 +13,48 @@ if(!isset($_SESSION['email']) & empty($_SESSION['email'])){
 <div class="close-btn fa fa-times"></div>
 
 
-<!-- SHOP CONTENT -->
 <section id="content">
     <div class="content-blog">
         <div class="container">
-            <div class="row">
-                <div class="page_header text-center">
-                    <h2>Shop</h2>
-                    <p>You can order products from here</p>
-                </div>
-                <div class="col-md-12">
-                    <div class="row">
-                        <div id="shop-mason" class="shop-mason-4col">
-                            <div class="sm-item isotope-item">
-                                <div class="product">
-                                    <div class="product-thumb">
-                                        <img src="images/shop/1.jpg" class="img-responsive" alt="">
-                                        <div class="product-overlay">
-												<span>
-												<a href="./shop-single-full.html" class="fa fa-link"></a>
-												<a href="./shop-single-full.html" class="fa fa-shopping-cart"></a>
-												</span>
-                                        </div>
-                                    </div>
-                                    <div class="rating">
-                                        <span class="fa fa-star act"></span>
-                                        <span class="fa fa-star act"></span>
-                                        <span class="fa fa-star act"></span>
-                                        <span class="fa fa-star act"></span>
-                                        <span class="fa fa-star act"></span>
-                                    </div>
-                                    <h2 class="product-title"><a href="#">Shave Knives</a></h2>
-                                    <div class="product-price">$79.00<span>$200.00</span></div>
-                                </div>
-                            </div>
+            <table class="table table-striped">
+                <thead>
+                    <tr>
+                        <th>#</th>
+                        <th>Customer Name</th>
+                        <th>Total Price</th>
+                        <th>Order Status</th>
+                        <th>Payment Mode</th>
+                        <th>Order Placed On</th>
+                        <th>Operations</th>
+</tr>
+</thead>
+<tbody>
+<?php
+// The logic here is showing the order item in the admin part
+$sql = "SELECT o.id, o.totalprice, o.orderstatus, o.paymentmode, o.timestamp, u.firstname, u.lastname 
+        FROM orders o 
+        JOIN usersmeta u ON o.uid = u.uid ORDER BY o.id DESC";
+$res = mysqli_query($connection, $sql);
 
-                        </div>
-                    </div>
-                    <div class="clearfix"></div>
-                    <!-- Pagination -->
-                    <div class="page_nav">
-                        <a href=""><i class="fa fa-angle-left"></i></a>
-                        <a href="" class="active">1</a>
-                        <a href="">2</a>
-                        <a href="">3</a>
-                        <a class="no-active">...</a>
-                        <a href="">9</a>
-                        <a href=""><i class="fa fa-angle-right"></i></a>
-                    </div>
-                    <!-- End Pagination -->
-                </div>
-            </div>
-        </div>
-    </div>
+while ($r = mysqli_fetch_assoc($res)) {
+    // Your logic for processing each row goes here
+
+?>
+
+    <tr>
+        <th scope="row"><?php echo $r['id']; ?></th>
+        <td><?php echo $r['firstname']. " " . $r['lastname']; ?></td>
+        <td><?php echo $r['totalprice']; ?></td>
+        <td><?php echo $r['orderstatus']; ?></td>
+        <td><?php echo $r['paymentmode']; ?></td>
+        <td><?php echo $r['timestamp']; ?></td>
+        <td><a href="editproduct.php?id=<?php echo $r['id']; ?>">Edit</a> | <a href="delproduct.php?id=<?php echo $r['id']; ?>">Delete</a></td>
+</tr>
+<?php } ?>
+</tbody>
+</table>
+</div>
+</div>
 </section>
-
 
 <?php include 'inc/footer.php' ?>
